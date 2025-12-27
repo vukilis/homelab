f41b42b3cd0247a5306ec3dc278082faea99b276
vukilis <vuk.lekic@vukilis.com>
2025-12-27 16:59:19 +0100

    27.12.2025

--- terraform/proxmox/live/containers/main.tf
+++ terraform/proxmox/live/containers/main.tf
@@ -78,3 +78,44 @@ module "test-container-1" {
     # Remote Exec Provisioner to enable root SSH login
     pve_connection = var.pve_connection
 }
+
+module "stremio" {
+    source = "../../modules/proxmox_lxc"
+
+    ssh_public_keys = file(pathexpand("~/.ssh/vuk.lekic.pub"))
+    target_node     = "homelab-vukilis"
+    ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+    start_at_boot   = true
+
+    vmid          = 229
+    root_password = var.root_password
+    unprivileged  = "true"
+
+    # Resource Allocation
+    cores     = 2
+    cpu_limit = 0
+    cpu_units = 100
+    memory    = 2048
+    swap      = 0
+    disk_size = "10G"
+    storage   = "local-lvm"
+
+    # Network Settings
+    network_name   = "eth0"
+    network_bridge = "vmbr0"
+    ip_address     = "192.168.0.229/24"
+    gateway        = "192.168.0.1"
+
+    # Identity & DNS
+    hostname     = "stremio"
+    searchdomain = "adguard.vukilis.com"
+    nameserver   = "192.168.0.202"
+
+    # Other Settings
+    onboot  = true
+    nesting = true
+    keyctl  = false
+
+    # Remote Exec Provisioner to enable root SSH login
+    pve_connection = var.pve_connection
+}
