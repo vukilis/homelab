c9c80d24b9f6bb126d87894a3c49f0e7df33ae2a
vukilis <vuk.lekic@vukilis.com>
2025-12-29 00:56:41 +0100

    29.12.2025

--- terraform/proxmox/live/containers/main.tf
+++ terraform/proxmox/live/containers/main.tf
@@ -42,18 +42,18 @@ module "alpine-it-tools" {
     keyctl  = false
 }
 
-module "test-container-0" {
+module "n8n" {
     source = "../../modules/proxmox_lxc"
 
     enable_ssh_hardening = true
     alpine_ssh_hardening = false
     pve_connection = var.pve_connection
     ssh_public_keys = file(pathexpand("~/.ssh/vuk.lekic.pub"))
-    target_node     = "pve"
+    target_node     = "homelab-vukilis"
     ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
     start_at_boot   = true
 
-    vmid          = 100
+    vmid          = 229
     root_password = var.root_password
     unprivileged  = "true"
 
@@ -61,8 +61,8 @@ module "test-container-0" {
     cores     = 2
     cpu_limit = 0
     cpu_units = 100
-    memory    = 512
-    swap      = 512
+    memory    = 2048
+    swap      = 0
     disk_size = "10G"
     storage   = "local-lvm"
 
@@ -73,59 +73,16 @@ module "test-container-0" {
     gateway        = "192.168.0.1"
 
     # Identity & DNS
-    hostname     = "test-container-0"
+    hostname     = "n8n"
     searchdomain = "adguard.vukilis.com"
     nameserver   = "192.168.0.202"
-    tags         = ["ct", "test"]
+    tags         = ["ct", "management"]
 
     # Other Settings
     onboot  = true
     nesting = true
     keyctl  = false
 }
-module "test-container-1" {
-    source = "../../modules/proxmox_lxc"
-
-    enable_ssh_hardening = true
-    alpine_ssh_hardening = false
-    pve_connection = var.pve_connection
-    ssh_public_keys = file(pathexpand("~/.ssh/vuk.lekic.pub"))
-    target_node     = "pve"
-    ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
-    start_at_boot   = true
-
-    vmid          = 101
-    root_password = var.root_password
-    unprivileged  = "true"
-
-    # Resource Allocation
-    cores     = 4
-    cpu_limit = 0
-    cpu_units = 100
-    memory    = 1024
-    swap      = 0
-    disk_size = "10G"
-    storage   = "local-lvm"
-
-    # Network Settings
-    network_name   = "eth0"
-    network_bridge = "vmbr0"
-    ip_address     = "192.168.0.230/24"
-    gateway        = "192.168.0.1"
-
-    # Identity & DNS
-    hostname     = "test-container-1"
-    searchdomain = "adguard.vukilis.com"
-    nameserver   = "192.168.0.202"
-    tags         = ["ct", "test"]
-
-    # Other Settings
-    onboot  = true
-    nesting = true
-    keyctl  = false
-}
-
-# Test containers for homelab-vukilis node
 
 module "stremio" {
     source = "../../modules/proxmox_lxc"
@@ -162,6 +119,91 @@ module "stremio" {
     searchdomain = "adguard.vukilis.com"
     nameserver   = "192.168.0.202"
     tags         = ["ct", "media"]
+
+    # Other Settings
+    onboot  = true
+    nesting = true
+    keyctl  = false
+}
+
+# Test containers for homelab-vukilis node
+
+module "test-container-0" {
+    source = "../../modules/proxmox_lxc"
+
+    enable_ssh_hardening = true
+    alpine_ssh_hardening = false
+    pve_connection = var.pve_connection
+    ssh_public_keys = file(pathexpand("~/.ssh/vuk.lekic.pub"))
+    target_node     = "pve"
+    ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+    start_at_boot   = true
+
+    vmid          = 301
+    root_password = var.root_password
+    unprivileged  = "true"
+
+    # Resource Allocation
+    cores     = 2
+    cpu_limit = 0
+    cpu_units = 100
+    memory    = 1024
+    swap      = 512
+    disk_size = "10G"
+    storage   = "local-lvm"
+
+    # Network Settings
+    network_name   = "eth0"
+    network_bridge = "vmbr0"
+    ip_address     = "192.168.0.253/24"
+    gateway        = "192.168.0.1"
+
+    # Identity & DNS
+    hostname     = "test-container-0"
+    searchdomain = "adguard.vukilis.com"
+    nameserver   = "192.168.0.202"
+    tags         = ["ct", "test"]
+
+    # Other Settings
+    onboot  = true
+    nesting = true
+    keyctl  = false
+}
+module "test-container-1" {
+    source = "../../modules/proxmox_lxc"
+
+    enable_ssh_hardening = true
+    alpine_ssh_hardening = false
+    pve_connection = var.pve_connection
+    ssh_public_keys = file(pathexpand("~/.ssh/vuk.lekic.pub"))
+    target_node     = "pve"
+    ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+    start_at_boot   = true
+
+    vmid          = 302
+    root_password = var.root_password
+    unprivileged  = "true"
+
+    # Resource Allocation
+    cores     = 2
+    cpu_limit = 0
+    cpu_units = 100
+    memory    = 1024
+    swap      = 0
+    disk_size = "1G"
+    storage   = "local-lvm"
+
+    # Network Settings
+    network_name   = "eth0"
+    network_bridge = "vmbr0"
+    ip_address     = "192.168.0.252/24"
+    gateway        = "192.168.0.1"
+
+    # Identity & DNS
+    hostname     = "test-container-1"
+    searchdomain = "adguard.vukilis.com"
+    nameserver   = "192.168.0.202"
+    tags         = ["ct", "test"]
 
     # Other Settings
     onboot  = true
