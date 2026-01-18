19f4dfd951a85d102f1ad7885963b5088709e23c
vukilis <vuk.lekic@vukilis.com>
2026-01-18 16:30:19 +0100

    added gotify

--- terraform/proxmox/live/containers/main.tf
+++ terraform/proxmox/live/containers/main.tf
@@ -965,7 +965,7 @@ module "smokeping" {
     cpu_units = 100
     memory    = 512
     swap      = 0
-    disk_size = "3G"
+    disk_size = "5G"
     storage   = "local-lvm"
 
     # Network Settings
@@ -976,6 +976,48 @@ module "smokeping" {
 
     # Identity & DNS
     hostname     = "smokeping"
+    searchdomain = "adguard.vukilis.com"
+    nameserver   = "192.168.0.202 1.1.1.1 9.9.9.9"
+    tags         = ["ct", "terraform", "monitoring"]
+
+    # Other Settings
+    onboot  = true
+    nesting = true
+    keyctl  = false
+}
+
+module "gotify" {
+    source = "../../modules/proxmox_lxc"
+
+    enable_ssh_hardening = true
+    alpine_ssh_hardening = false
+    pve_connection = var.pve_connection
+    ssh_public_keys = file(pathexpand("~/.ssh/vuk.lekic.pub"))
+    target_node     = "homelab-vukilis"
+    ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+    start_at_boot   = true
+
+    vmid          = 228
+    root_password = var.root_password
+    unprivileged  = "true"
+
+    # Resource Allocation
+    cores     = 1
+    cpu_limit = 0
+    cpu_units = 100
+    memory    = 512
+    swap      = 0
+    disk_size = "5G"
+    storage   = "local-lvm"
+
+    # Network Settings
+    network_name   = "eth0"
+    network_bridge = "vmbr0"
+    ip_address     = "192.168.0.228/24"
+    gateway        = "192.168.0.1"
+
+    # Identity & DNS
+    hostname     = "gotify"
     searchdomain = "adguard.vukilis.com"
     nameserver   = "192.168.0.202 1.1.1.1 9.9.9.9"
     tags         = ["ct", "terraform", "monitoring"]
--- terraform/proxmox/live/containers/outputs.tf
+++ terraform/proxmox/live/containers/outputs.tf
@@ -32,6 +32,7 @@ output "all_containers" {
             "filebrowser"      = try(module.filebrowser, null)  
             "netalertx"        = try(module.netalertx, null)  
             "smokeping"        = try(module.smokeping, null)  
+            "gotify"           = try(module.gotify, null)  
             
             "test-container-0" = try(module.test-container-0, null)
             "test-container-1" = try(module.test-container-1, null)
